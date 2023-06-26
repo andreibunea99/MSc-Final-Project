03b62a73d45e0d523d2d036ad58e708b1891b23f
andreibunea99 <56512139+andreibunea99@users.noreply.github.com>
2023-06-26 20:14:17 +0100

    Model Preview FINISHED!!

--- frontend/src/app/model-preview/model-preview.component.ts
+++ frontend/src/app/model-preview/model-preview.component.ts
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-model-preview',
@@ -12,100 +13,111 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader';
 export class ModelPreviewComponent {
   @ViewChild('modelContainer', { static: true }) modelContainer!: ElementRef;
 
+  private container!: HTMLElement;
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private renderer!: THREE.WebGLRenderer;
+  private controls!: OrbitControls;
+  private model!: THREE.Group;
+
   ngAfterViewInit(): void {
-    const container = this.modelContainer.nativeElement;
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
+    this.container = this.modelContainer.nativeElement;
+
+    this.initScene();
+    this.loadModel();
+    this.addLights();
+    this.addControls();
+    this.animate();
+  }
+
+  private initScene(): void {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
 
-    let model: THREE.Mesh | undefined; // Variable to hold the loaded model
-    let loadedTextures: number = 0; // Counter for loaded textures
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.container.appendChild(this.renderer.domElement);
 
+    this.camera.position.z = 5;
+
+    window.addEventListener('resize', this.handleResize.bind(this));
+  }
+
+  private loadModel(): void {
+    const objLoader = new OBJLoader();
+    const mtlLoader = new MTLLoader();
     const textureLoader = new TextureLoader();
-
-    const checkTexturesLoaded = (totalTextures: number) => {
-      loadedTextures++;
-      if (loadedTextures === totalTextures) {
-        // All textures loaded, proceed with rendering
-        animate();
-      }
-    };
-
-    const animate = function () {
-      requestAnimationFrame(animate);
-
-      // Rotate the model on multiple axes
-      if (model) {
-        model.rotation.x += 0.005; // Rotate around the x-axis
-        model.rotation.y += 0.0025; // Rotate around the y-axis
-        model.rotation.z += 0.00125; // Rotate around the z-axis
-      }
-
-      renderer.render(scene, camera);
-    };
-
-    camera.position.z = 5;
-
     const modelData = localStorage.getItem('model');
     if (modelData) {
       const modelInfo = JSON.parse(modelData);
-      const objLoader = new OBJLoader();
-      const mtlLoader = new MTLLoader();
 
       mtlLoader.load(modelInfo.mtl, (materials) => {
         materials.preload();
         objLoader.setMaterials(materials);
+
         objLoader.load(modelInfo.obj, (object) => {
-          object.traverse((node) => {
+          this.model = object as THREE.Group;
+          this.scene.add(this.model);
+
+          // Traverse through the loaded model to assign textures
+          this.model.traverse((node) => {
             if (node instanceof THREE.Mesh) {
-              const mesh = node as THREE.Mesh;
+              const mesh = node;
+              const material = mesh.material as THREE.MeshPhongMaterial;
 
-              const customMaterial = new THREE.MeshPhongMaterial();
-
-              const totalTextures = modelInfo.textures.length;
-
-              // Load and apply textures to the material
-              modelInfo.textures.forEach((textureInfo: any) => {
-                const texture = textureLoader.load(
-                  textureInfo.path,
-                  () => checkTexturesLoaded(totalTextures) // Texture onLoad callback
-                );
-                const propertyName = textureInfo.propertyName;
-
-                // Set the texture map based on the property name
-                switch (propertyName) {
-                  case 'map':
-                    customMaterial.map = texture;
-                    break;
-                  case 'normalMap':
-                    customMaterial.normalMap = texture;
-                    break;
-                  // Add more cases for other texture maps (e.g., roughnessMap, metalnessMap, etc.)
-                }
-              });
-
-              mesh.material = customMaterial;
-              model = mesh; // Assign the loaded model to the variable
+              if (material.map) {
+                const texturePath = material.map.image.src;
+                const texture = textureLoader.load(texturePath, () => {
+                  material.needsUpdate = true;
+                  this.renderer.render(this.scene, this.camera);
+                });
+                material.map = texture;
+              }
             }
           });
-
-          scene.add(object);
         });
       });
-
-      // Add directional light to the scene
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-      directionalLight.position.set(1, 1, 1);
-      scene.add(directionalLight);
-
-      animate();
     }
+  }
+
+  private addLights(): void {
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 1, 1);
+    this.scene.add(directionalLight);
+  }
+
+  private addControls(): void {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.05;
+    this.controls.rotateSpeed = 0.5;
+    this.controls.zoomSpeed = 1.2;
+    this.controls.addEventListener('change', this.render.bind(this));
+  }
+
+  private handleResize(): void {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    this.renderer.setSize(width, height);
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+
+    this.render();
+  }
+
+  private animate(): void {
+    requestAnimationFrame(this.animate.bind(this));
+    this.controls.update();
+    this.render();
+  }
+
+  private render(): void {
+    this.renderer.render(this.scene, this.camera);
   }
 }
